7e716851d90c113f548a8f08e5166a4ca213bc76
michael2221807 <michael2221807@gmail.com>
2021-01-31 02:26:51 -0500

    Add files via upload

--- responsive-table-less-shopping-cart/dist/script.js
+++ responsive-table-less-shopping-cart/dist/script.js
@@ -6,6 +6,16 @@ $('a.remove').click(function(){
 })
 
 // Just for testing, show all items
-  $('a.btn.continue').click(function(){
+  $('checkout').click(function(){
+  	console.log('continue...')
     $('li.items').show(400);
   })
+  
+
+ //get count from items
+
+ function getCount() {
+ 	var count = 0;
+ 	count = document.getElementById("first").value;
+ 	console.log("Count: " + count);
+ }
